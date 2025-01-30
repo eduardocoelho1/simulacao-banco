0b43fa9a5a97dd548c9400565e78e62f06432904
Eduardo Coelho <eduardo.coelho1@ufla.estudante.br>
2025-01-29 22:47:12 -0300

    exceção removida

--- TooManyATMsException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exceção não verificada de quantidade de caixas maior que a suportada.
- */
-public class TooManyATMsException extends RuntimeException {
-    public TooManyATMsException(String errorMessage) {
-        super(errorMessage);
-    }
-}
